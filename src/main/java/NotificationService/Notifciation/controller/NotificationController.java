3b766b3b851abc5f66d8e34d7df2d671d1a338fe
aniksinha <aniksinha1999@gmail.com>
2026-07-11 13:13:15 +0530

    Notification Commit push

--- src/main/java/NotificationService/Notifciation/controller/NotificationController.java
+++ src/main/java/NotificationService/Notifciation/controller/NotificationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
+
 public class NotificationController {
     private final  NotificationService notificationService;
 
